409bc0adf8a1f0188f7c21323a6b3e61b29aa934
spmani99 <sandaleepamoda@gmail.com>
2024-03-22 00:15:23 +0530

    hello method

--- src/main/java/com/springboot/todowebapp/hello/SayHelloController.java
+++ src/main/java/com/springboot/todowebapp/hello/SayHelloController.java
@@ -1,5 +1,15 @@
 package com.springboot.todowebapp.hello;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class SayHelloController {
 
+	@RequestMapping("say-hello")
+	@ResponseBody
+	public String sayHello() {
+		return "Hi Sandali";
+	}
 }
