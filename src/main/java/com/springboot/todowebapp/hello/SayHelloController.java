f09e8709af9b72ff0d1b04fbbc85a04c57b013fa
spmani99 <sandaleepamoda@gmail.com>
2024-03-22 02:18:22 +0530

    JSP setup

--- src/main/java/com/springboot/todowebapp/hello/SayHelloController.java
+++ src/main/java/com/springboot/todowebapp/hello/SayHelloController.java
@@ -2,14 +2,19 @@ package com.springboot.todowebapp.hello;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+//import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class SayHelloController {
 
+//	@RequestMapping("say-hello")
+//	@ResponseBody
+//	public String sayHello() {
+//		return "Hi Sandali";
+//	}
+	
 	@RequestMapping("say-hello")
-	@ResponseBody
 	public String sayHello() {
-		return "Hi Sandali";
+		return "sayHello";
 	}
 }
